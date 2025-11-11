e68bfede8dcfae8134a5b03d102f307e5b3bd09d
GDD-impact <gddimpactsoftwares@gmail.com>
2025-11-11 12:09:42 +0100

    update

---
+++ app/gallery/page.tsx
@@ -0,0 +1,10 @@
+import Gallery from '@/components/modules/gallery/container'
+import React from 'react'
+
+const GalleryPage = () => {
+  return (
+    <Gallery />
+  )
+}
+
+export default GalleryPage
